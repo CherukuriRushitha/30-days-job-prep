04e333d27f6562b9e0600712d48164f98ecb174c
Rushitha Cherukuri <rushithacherukuri07@gmail.com>
2026-08-05 13:09:48 +0530

    Day 1: Setup development environment and first Javascript program

---
+++ javascript/day1.js
@@ -0,0 +1,8 @@
+console.log("Hello, Rushitha!");
+console.log("Day 1 of my Software Engineer Journey 🚀");
+
+let name = "Rushitha";
+let goal = "Get a Software Developer Job";
+
+console.log(`Hi, I'm ${name}.`);
+console.log(`My goal is to ${goal}.`);
