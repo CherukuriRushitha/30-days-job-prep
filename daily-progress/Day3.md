71860e71704d51fc147c101f28c967daefb7e719
Rushitha Cherukuri <rushithacherukuri07@gmail.com>
2026-08-07 18:09:53 +0530

    Day3: Conditional statements, logical operators and switch

---
+++ daily-progress/Day3.md
@@ -0,0 +1,7 @@
+#Day 3
+##Learned
+-If else statements
+-Switch statement
+
+##built
+-Grade Calculator
---
+++ javascript/day3-grade-calculator.js
@@ -0,0 +1,14 @@
+let marks = 85;
+if (marks < 0 || marks > 100){
+    console.log("Invalid Marks")
+}else if (marks >= 90){
+    console.log("Grade A");
+}else if (marks >= 75 ){
+    console.log("Grade B");
+}else if (marks >= 50 ){
+    console.log("Grade C");
+}else if (marks >= 35 ){
+    console.log("Pass");
+}else{
+    console.log("Fail");
+}
